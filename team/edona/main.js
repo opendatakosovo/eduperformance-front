6b0f6f435ac95e97d9f6e34768e6355ee932c1c0
EdonaHaziraj <39129202+EdonaHaziraj@users.noreply.github.com>
2019-01-31 00:47:17 +0100

    Search and Login Form - done

--- team/edona/main.js
+++ team/edona/main.js
@@ -1,3 +1,34 @@
-$(document).ready(function() {
-    //your code
-} );
+/*Change the color of form input types to blue, after clicked.*/
+/*3 functions of search form*/
+function FteacherOption() {
+	document.getElementById("teacherOption").style.color = "#0067f4";
+	document.getElementById("cityOption").style.color = "#999798";
+	document.getElementById("schoolOption").style.color = "#999798";
+
+}
+
+
+function FcityOption() {
+	document.getElementById("cityOption").style.color = "#0067f4";
+	document.getElementById("teacherOption").style.color = "#999798";
+	document.getElementById("schoolOption").style.color = "#999798";
+}
+
+
+function FschoolOption() {
+	document.getElementById("schoolOption").style.color = "#0067f4";
+	document.getElementById("teacherOption").style.color = "#999798";
+	document.getElementById("cityOption").style.color = "#999798";
+}
+
+/*2 functions of login form*/
+
+function FEmailOption() {
+	document.getElementById("emailOption").style.color = "#0067f4";
+	document.getElementById("passOption").style.color = "#999798";
+}
+
+function FpassOption() {
+	document.getElementById("passOption").style.color = "#0067f4";
+	document.getElementById("emailOption").style.color = "#999798";
+}
