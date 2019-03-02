5c9d7d8567ee6a14be3ebadea43277fed1e1c9ea
EdonaHaziraj <39129202+EdonaHaziraj@users.noreply.github.com>
2019-03-02 10:55:13 +0100

    emoji first release

---
+++ team/edona/slidertask/javascript.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $("#volume").slider({
+        min: 0,
+        max: 100,
+        value: 0,
+        range: "min",
+        slide: function (event, ui) {
+            changeSlider(ui.value);
+        }
+    });
+    var myMedia = document.createElement('audio');
+    $('#player').append(myMedia);
+    myMedia.id = "myMedia";
+
+    playAudio('http://a.tumblr.com/tumblr_l14pkbmbuq1qavyj3o1.mp3', 0);
+
+    function playAudio(fileName, myVolume) {
+        myMedia.src = fileName;
+        myMedia.setAttribute('loop', 'loop');
+        setVolume(myVolume);
+        myMedia.play();
+    }
+
+    function setVolume(myVolume) {
+        var myMedia = document.getElementById('myMedia');
+        myMedia.volume = myVolume;
+    }
+
+    function changeSlider(myVolume) {
+        let volumeThreshold = [15, 30, 45, 60, 70, 80, 96, 101]
+        let volumeClasses = ["v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7"]
+        let classIndex = volumeThreshold.findIndex((threshold) => {
+            return myVolume < threshold
+        });
+        changeClassTo(volumeClasses[classIndex]);
+        setVolume(myVolume / 100);
+    }
+
+    function changeClassTo(className) {
+        $('#volume').removeClass();
+        $('#volume').addClass(className);
+    }
+});
+
+
