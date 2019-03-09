d976a0dae8c15203ace394ac741d0f0e44da996e
Driton Alija <dritonalija@hotmail.com>
2019-03-09 17:22:55 +0100

    report, ask, statistics

--- release/assets/js/main.js
+++ release/assets/js/main.js
@@ -1,15 +1,27 @@
 $(document).ready(function () {
-   /* $(".shade").mouseover(function () {
-        $(this).addClass("boxShadow ");
-        $(this).addClass("hovered");
-        $(this).find(".imgSch2").addClass("hovered");
-        $(this).find(".color").addClass("customFontCards2");
+    /* $(".shade").mouseover(function () {
+         $(this).addClass("boxShadow ");
+         $(this).addClass("hovered");
+         $(this).find(".imgSch2").addClass("hovered");
+         $(this).find(".color").addClass("customFontCards2");
+     });
+     $(".shade").mouseout(function () {
+         $(this).removeClass("boxShadow ");
+         $(this).removeClass("hovered");
+         $(this).find(".imgSch2").removeClass("hovered");
+         $(this).find(".color").removeClass("customFontCards2");
+     })*/
+    $(".ask-input").focus(function () {
+        var editid = $(this).attr('id');
+        var formclass = "ask-input-focus-" + editid;
+        $(this).css('cssText', "border-color:#0067f4 !important;");
+        $("." + formclass).css('cssText', "color: #0067f4 !important;");
     });
-    $(".shade").mouseout(function () {
-        $(this).removeClass("boxShadow ");
-        $(this).removeClass("hovered");
-        $(this).find(".imgSch2").removeClass("hovered");
-        $(this).find(".color").removeClass("customFontCards2");
-    })*/
+    $(".ask-input").focusout(function () {
+        var editid = $(this).attr('id');
+        var formclass = "ask-input-focus-" + editid;
+        $(this).css('cssText', "border-color:#a4a4a4 !important;");
+        $("." + formclass).css('cssText', "color: black;");
+    });
 
 });
